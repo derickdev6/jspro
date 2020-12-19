0ce230765ac9df3b8c80818e2ece3669cf0802b7
Derick Robinson <derickdev6@gmail.com>
2020-12-19 01:10:18 -0500

    Cap 7 and challenge done

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,12 @@
 import MediaPlayer from './MediaPlayer.js';
+import Autoplay from './plugins/Autoplay.js'
 
 const video = document.querySelector(`video`);
-const player = new MediaPlayer({el:video});
-const btn = document.querySelector(`button`);
+const player = new MediaPlayer({el:video, plugins: [
+    new Autoplay()
+] });
+const btnplay = document.getElementById('play');
+const btnmute = document.getElementById('mute');
 
-btn.onclick = () => player.togglePlay();
+btnplay.onclick = () => player.togglePlay();
+btnmute.onclick = () => player.toggleMute();
